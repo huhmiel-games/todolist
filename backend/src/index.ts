369f1514210dd9df857a479926fa7158c798d956
philippe Pereira <phil.pereira@protonmail.com>
2024-02-02 15:45:15 +0100

    feat: basic api routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,24 +2,101 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import Database from 'better-sqlite3';
 import path from "path";
+import { isTask } from "./typeguards";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
-const dbPath = path.join(__dirname, process.env.DB_PATH || './database/todo.db') 
+const dbPath = path.join(__dirname, process.env.DB_PATH || './database/todo.db')
 
 const db = new Database(dbPath, { verbose: console.log });
 db.pragma('journal_mode = WAL');
 
 app.use(express.json());
 
-
+// return all todos 
 app.get("/", (req: Request, res: Response) =>
 {
-    const data = db.prepare('SELECT * FROM tasks').all();
-    res.json(data);
+    try
+    {
+        const data = db.prepare('SELECT * FROM tasks ORDER BY id DESC, done ASC').all();
+
+        res.json(data ?? []);
+    }
+    catch (error)
+    {
+        res.status(500).json();
+    }
+});
+
+// return one todo
+app.get("/:id", (req: Request, res: Response) =>
+{
+    try
+    {
+        const data = db.prepare('SELECT * FROM tasks WHERE id = ?').get(req.params.id);
+
+        if (data !== undefined)
+        {
+            res.status(200).json(data);
+        }
+        else
+        {
+            res.status(404).json({ error: 'no task with this id' });
+        }
+    }
+    catch (error)
+    {
+        res.status(500).json();
+    }
+});
+
+// add a new todo
+app.post("/", (req: Request, res: Response) =>
+{
+    try
+    {
+        const data = db.prepare('INSERT INTO tasks ("title", "description", "done") VALUES (?, ?, ?)')
+            .run(req.body.title, req.body.description ?? "", req.body.done ?? 0);
+
+        res.status(201).json(data);
+    }
+    catch (err)
+    {
+        if (err instanceof Database.SqliteError && err.code === 'SQLITE_CONSTRAINT_NOTNULL')
+        {
+            res.status(400).json({ error: 'title is missing' });
+        }
+        else
+        {
+            res.status(500).json();
+        }
+    }
+});
+
+// toggle a todo state
+app.patch("/:id", (req: Request, res: Response) =>
+{
+    try
+    {
+        const data = db.prepare('SELECT * FROM tasks WHERE id = ?').get(req.params.id);
+
+        if (isTask(data))
+        {
+            db.prepare('UPDATE tasks SET done = ? WHERE id = ?').run(data.done === 0 ? 1 : 0, req.params.id);
+            res.status(204).json();
+        }
+        else
+        {
+            res.status(404).json({ error: 'no task with this id' });
+        }
+    }
+    catch (error)
+    {
+        res.status(500).json();
+    }
 });
 
 app.listen(port, () =>
